d1e716c745873d0edfda46b2b629c294805c1681
Higor Ericson <89282605+Higorericson@users.noreply.github.com>
2024-01-07 13:44:37 -0300

    Enviando a nova versão do projeto

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone nokia:");
+Nokia nokia = new Nokia("12345", "1", "111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iphone:");
+Smartphone iphone = new Iphone("4582", "3", "4444444", 524);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("telegram");
